33e36e2476157e9a444bb4b7bca31e650bfd6938
FionaMacRaeFairlie <fionamacraefairlie@gmail.com>
2022-02-03 15:05:48 +0000

    updated Lab3a

---
+++ app.js
@@ -0,0 +1,94 @@
+var sqlite3 = require('sqlite3').verbose();
+var express = require('express');
+var http = require('http');
+var path = require("path");
+
+var app = express();
+var server = http.createServer(app);
+
+let db = new sqlite3.Database('./database/employees.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+      console.error(err.message);
+    } else
+    console.log('Connected to the employees database.');
+  });
+
+app.use(express.urlencoded({extended: false }));
+app.use(express.static(path.join(__dirname,'./public')));
+
+db.run('CREATE TABLE IF NOT EXISTS emp(id TEXT, name TEXT)');
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname,'./public/index.html'));
+  });
+
+// Insert
+app.post('/add', function(req,res){
+    db.serialize(()=>{
+      db.run('INSERT INTO emp(id,name) VALUES(?,?)', [req.body.id, req.body.name], function(err) {
+        if (err) {
+          return console.log(err.message);
+        }
+        console.log("New employee has been added");
+        res.sendFile(path.join(__dirname,'./public/index.html'));
+      });
+  });
+  });
+  
+  // View
+  app.post('/view', function(req,res){
+    db.serialize(()=>{
+      db.each('SELECT id ID, name NAME FROM emp WHERE id =?', [req.body.id], function(err,row){     //db.each() is only one which is funtioning while reading data from the DB
+        if(err){
+          res.send("Error encountered while displaying");
+          return console.error(err.message);
+        }
+        res.send(` ID: ${row.ID},    Name: ${row.NAME}`);
+        console.log("Entry displayed successfully");
+      });
+    });
+  });
+
+//Update
+app.post('/update', function(req,res){
+    db.serialize(()=>{
+      db.run('UPDATE emp SET name = ? WHERE id = ?', [req.body.name,req.body.id], function(err){
+        if(err){
+          res.send("Error encountered while updating");
+          return console.error(err.message);
+        }
+        res.send("Entry updated successfully");
+        console.log("Entry updated successfully");
+      });
+    });
+  });
+ 
+  //Delete
+app.post('/delete', function(req,res){
+    db.serialize(()=>{
+      db.run('DELETE FROM emp WHERE id = ?', req.body.id, function(err) {
+        if (err) {
+          res.send("Error encountered while deleting");
+          return console.error(err.message);
+        }
+        res.send("Entry deleted");
+        console.log("Entry deleted");
+      });
+    });
+  });
+
+  //Close
+  app.get('/close', function(req,res){
+    db.close((err) => {
+      if (err) {
+        res.send('There is some error in closing the database');
+        return console.error(err.message);
+      }
+      console.log('Closing the database connection.');
+      res.send('Database connection successfully closed');
+    });
+  });
+
+  server.listen(3000,function(){ 
+    console.log("Server listening on port: 3000");
+  });
